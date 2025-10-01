5973945dedb99ebd08b4b8a9dd6dd110bcb41681
Gagun45 <selyanchyn45@gmail.com>
2025-10-01 18:18:46 +0300

    profile page responsivness

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -40,30 +40,34 @@ const ProfilePage = () => {
   return (
     <main className="space-y-8">
       <h1>Profile page</h1>
-      <Card>
-        <CardContent className="space-y-4 w-72">
-          <div className="relative size-36 lg:size-48 mx-auto">
-            <Image
-              priority
-              src={
-                user.UserInformation?.avatarUrl ?? user.image ?? DefaultAvatar
-              }
-              alt="Profile avatar"
-              sizes="(max-width: 640px) 144px, 192px"
-              fill
-            />
-          </div>
-          <h2 className="text-center">{user.email}</h2>
-        </CardContent>
-      </Card>
-      <Card className="w-full">
-        <CardHeader>
-          <CardTitle className="tracking-wider">Profile Information</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <EditProfileForm />
-        </CardContent>
-      </Card>
+      <div className="flex flex-col items-center w-full gap-12 lg:flex-row lg:justify-center lg:items-start">
+        <Card className="w-full max-w-2xl lg:w-72 shrink-0">
+          <CardContent className="space-y-4">
+            <div className="relative size-36 lg:size-48 mx-auto">
+              <Image
+                priority
+                src={
+                  user.UserInformation?.avatarUrl ?? user.image ?? DefaultAvatar
+                }
+                alt="Profile avatar"
+                sizes="(max-width: 640px) 144px, 192px"
+                fill
+              />
+            </div>
+            <h2 className="text-center break-words">{user.email}</h2>
+          </CardContent>
+        </Card>
+        <Card className="w-full max-w-2xl">
+          <CardHeader>
+            <CardTitle className="tracking-wider">
+              Profile Information
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <EditProfileForm />
+          </CardContent>
+        </Card>
+      </div>
     </main>
   );
 };
